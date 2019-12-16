d2f27a3f6f6cf9dbbc43ce7f7c70d22336fc7ef0
Globe-Eater <bullokl@ostatemail.okstate.edu>
2019-12-16 14:51:24 -0600

    unit test methods added and starting on objective for program.

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,26 +1,39 @@
 """ This program is for testing Spreadsheet-Combine.py"""
-import Combine_spreadsheets
 import unittest
 import pandas as pd
+import Combine_spreadsheets
+
 
 class test_sheets(unittest.TestCase):
-	
+	'''This class is for initialzing the unittest module apart of the python standard library.'''
+
 	def setup(self):
+		'''Place holder for habbit'''
 		pass
 
-	def tear_down(self):
+ 	def tear_down(self):
+		'''Place holder for habbit'''
 		pass
 
 	def single_sheet(self):
 		''' If only one sheet is put into the program will any changes occur?'''
-		df = pd.DataFrame(data=[[1,2,3],[4,5,6]])
+		df = pd.DataFrame(data=[[1, 2, 3],[4, 5, 6]])
 		df.to_excel('Test_file.xlsx')
 		result = Combine_spreadsheets.combine()
 		self.assertEqual(result, df)
 
 	def two_sheets(self):
 		'''This method tests to see if concatination occured'''
-		df = pd.DataFrame(data=[['Dog','Cat','Crab'],[1,2,3]])
-		df2 = pd.DataFrame(data=[['Dog','Cat','Crab'],[4,5,6]])
+		df1 = pd.read_excel('test_sheet_1.xlsx')
+		df2 = pd.read_excel('test_sheet_2.xlsx')
 		result = Combine_spreadsheets.combine()
-		self.assertEqual(result, 
+		self.assertEqual(result, pd.concat(df1, df2))
+
+	def all_sheets(self):
+	'''This method is for testing to see if all spreadsheets will be concated together
+	This is the objective of the program.'''
+	
+
+if __name__ == '__main__':
+	unit_test.main()
+
